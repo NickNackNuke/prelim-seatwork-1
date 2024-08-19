e56068d795bc90e54155e015dec2df1cf30d739d
Lorel Christian Beligaño <95603-25260308@SERVER2.DOMAIN>
2024-08-19 18:21:35 +0800

    First Commit

---
+++ sample.js
@@ -0,0 +1,12 @@
+const user = {
+    fname: "Jessica",
+    lname: " Cordero",
+    gender: " Male",
+    age: 30
+}
+let message = ""
+
+//bracket notation
+message = user['fname'] + user['lname']
+message1 = user['age']
+console.log("I am " + message + " and I am "+ message1 + " yrs old");
